3d2e1bd6e2236ba1e6618e140aef017cb68afef4
lucasobyszuk <lucasobyszuk@gmail.com>
2015-03-12 06:02:41 -0300

    Added practice 6

--- 03-Node-npm-modules/js/movies.js
+++ 03-Node-npm-modules/js/movies.js
@@ -1,7 +1,28 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-function Director() {
-	// Nothing to do here yet
+function Director(name) {
+	this.name = name;
+	this.quotes = [];
 }
+
+Director.prototype.speak = function() {
+	if (this.quotes.length > 0) {
+		console.log(this.name + ' says:');
+		for (var i = 0; i < this.quotes.length ; i++)
+			console.log(this.quotes[i]);
+	}
+	else {
+		console.log(this.name + 'has no quotes :S');
+	}
+	return this.quotes;
+};
+
+Director.prototype.set = function(attr,value) {
+	this[attr] = value;
+};
+
+Director.prototype.get = function(attr) {
+	return this[attr];
+};
 
 module.exports = Director;
 },{}],2:[function(require,module,exports){
